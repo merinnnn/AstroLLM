06daf9f3e9fce2a4089529dd167c048efc5edaae
merinnnn <merin1970@gmail.com>
2025-09-20 20:56:28 +0100

    update: formatting

--- src/nasa_api.py
+++ src/nasa_api.py
@@ -1,24 +1,25 @@
 import requests
-from config import NASA_API_KEY
+from .config import NASA_API_KEY
 
 def get_apod(date=None):
-    url = f"https://api.nasa.gov/planetary/apod"
+    url = "https://api.nasa.gov/planetary/apod"
     params = {
-        "api_key": NASA_API_KEY,
-        "date": date
+        "api_key": NASA_API_KEY
     }
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    return response.json()
+    if date: 
+        params["date"] = date
+    r = requests.get(url, params=params)
+    r.raise_for_status()
+    return r.json()
 
-def get_mars_rover_photos(sol=None, camera=None):
-    url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
+def get_mars_photos(sol=1000, rover="curiosity", camera=None):
+    url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rover}/photos"
     params = {
         "api_key": NASA_API_KEY,
-        "sol": sol,
-        "camera": camera
+        "sol": sol
     }
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    print(response)
-    return response.json()
+    if camera: 
+        params["camera"] = camera
+    r = requests.get(url, params=params)
+    r.raise_for_status()
+    return r.json()
